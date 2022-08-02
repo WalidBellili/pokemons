f4ca0531daec76f063b3d967383075a553c2356c
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-08-02 10:16:01 +0200

    card css

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import { random } from "lodash";
 import { useEffect, useState } from "react";
 // import Home from "./components/Home";
 
+import "./App.css";
+
 function App() {
   const [pokemons, setPokemons] = useState(null);
-
   const [randomPokemon, setRandomPokemon] = useState(
     "https://pokeapi.co/api/v2/pokemon/1"
   );
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [randomPokemon]);
 
   const fetchData = async () => {
     const request = await fetch(randomPokemon);
@@ -30,11 +31,11 @@ function App() {
 
   console.log(pokemons);
   return (
-    <div>
+    <main>
       <img
         src={pokemons.sprites.front_default}
         alt=""
-        width={150}
+        width={250}
         height={150}
       />
       <p>{pokemons.name}</p>
@@ -50,8 +51,8 @@ function App() {
           </ul>
         );
       })}
-      <button onClick={randomPokemonFunction}></button>
-    </div>
+      <button onClick={randomPokemonFunction}>Random pokemon</button>
+    </main>
   );
 }
 
